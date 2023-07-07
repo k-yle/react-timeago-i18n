80e9c6e22ccf8b222bf20ec9b4ac5a37ff357c2a
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-07-07 23:12:37 +1200

    update dependencies & set up tests

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/__tests__/setupTests.ts"],
+    include: ["**/*.test.*"],
+    globals: true,
+  },
+});
